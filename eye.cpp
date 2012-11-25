23cfaf5bed5f9fe57cbe1de14f4395584f3f3582
Martin Kielhorn <kielhorn.martin@googlemail.com>
2012-11-25 16:10:09 +0100

    extract images of the eyes

--- eye.cpp
+++ eye.cpp
@@ -7,41 +7,59 @@ int main(int argc,char**argv)
 {
   
   VideoCapture c(0);
+  if(!c.isOpened())
+    return -1;
   std::cout << "frame: " <<
     c.get(CV_CAP_PROP_FRAME_WIDTH) << " " <<
     c.get(CV_CAP_PROP_FRAME_HEIGHT) << std::endl;
-  c.set(CV_CAP_PROP_FRAME_WIDTH,352);
-  c.set(CV_CAP_PROP_FRAME_HEIGHT,288);
+  /*  c.set(CV_CAP_PROP_FRAME_WIDTH,352);
+      c.set(CV_CAP_PROP_FRAME_HEIGHT,288);
   
-  std::cout << "frame: " <<
-    c.get(CV_CAP_PROP_FRAME_WIDTH) << " " <<
-    c.get(CV_CAP_PROP_FRAME_HEIGHT) << std::endl;
-  
+      std::cout << "frame: " <<
+      c.get(CV_CAP_PROP_FRAME_WIDTH) << " " <<
+      c.get(CV_CAP_PROP_FRAME_HEIGHT) << std::endl;
+  */
   CascadeClassifier face;
   face.load("haarcascade_frontalface_default.xml");
-  namedWindow("bla",0); //CV_WINDOW_AUTOSIZE);
-    
+  namedWindow("bla",CV_WINDOW_AUTOSIZE);
+  namedWindow("left",CV_WINDOW_AUTOSIZE);
+  namedWindow("right",CV_WINDOW_AUTOSIZE);
+  Mat image;
   while(1){
-    Mat image;
     c >> image;
-    //  GaussianBlur(image,image,Size(9,9),2,2);
-    resize(image,image,Size(),.25,.25,INTER_NEAREST);
+    //GaussianBlur(image,image,Size(5,5),3,3);
     Mat g;
     cvtColor(image,g,CV_BGR2GRAY);
+    double scale=2;
+    Mat small( cvRound (g.rows/scale), cvRound(g.cols/scale), CV_8UC1 );
+    resize(g,small,small.size(),0,0,INTER_NEAREST);
+    //    equalizeHist(small,small);
     std::vector<Rect>faces;
-    if(0)face.detectMultiScale(g,faces,1.1,2,
-			  0|CV_HAAR_SCALE_IMAGE,Size(30,30));
-    if(0)for(int i=0;i<faces.size();i++){
-      float w=faces[i].width*.5,
-	h= faces[i].height*.5;
-      Point c(faces[i].x+w,faces[i].y+h);
-      ellipse(g,c,Size(w,h),0,0,360,Scalar(255,0,255),
-	      4,8,0);
-    }
-    imshow("bla",g);
+    face.detectMultiScale(small,faces,1.2,12,
+			  0
+			  |CV_HAAR_FIND_BIGGEST_OBJECT
+			  |CV_HAAR_DO_ROUGH_SEARCH
+			  |CV_HAAR_SCALE_IMAGE,Size(30,30));
+    if(faces.size()){
+      //      rectangle(small,faces[0],Scalar(255));
+      Rect left;
+      Point
+	rc=faces[0].tl()+Point(.7*faces[0].width,.4*faces[0].height),
+	lc=faces[0].tl()+Point(.3*faces[0].width,.4*faces[0].height),
+	s=Size(.3*.35*faces[0].width,.3*.3*faces[0].height);
+      rectangle(small,Rect(lc-s,lc+s),Scalar(255)); 
+      rectangle(small,faces[0],Scalar(255),3); 
+      rectangle(small,Rect(rc-s,rc+s),Scalar(255));
+      Mat left_eye(small,Rect(lc-s,lc+s)),
+	right_eye(small,Rect(rc-s,rc+s));
+      imshow("bla",small);
+      imshow("left",left_eye);
+      imshow("right",right_eye);
+    } else
+      imshow("bla",small);
+    
+    if(waitKey(10) >= 0) 
+      ;
   }
- 
-  
-  waitKey(0);
 }
 
